f3fd303fcfbdeba6dc3516abea3ddce616c27969
Peter Schrammel <peter.schrammel@metoda.com>
2021-04-25 20:12:44 +0000

    doesn't belong to users

--- db/migrate/20210229052417_create_providers.rb
+++ db/migrate/20210229052417_create_providers.rb
@@ -1,7 +1,6 @@
 class CreateProviders < ActiveRecord::Migration[5.1]
   def change
     create_table :oidc_providers do |t|
-      t.belongs_to :users
       t.string :issuer, :jwks_uri, :name, :identifier, :secret, :scopes_supported, :host, :scheme
       t.string :authorization_endpoint, :token_endpoint, :userinfo_endpoint
       t.boolean :dynamic, default: false
